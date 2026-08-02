a5190b1da26e58dc34b1d7710afe4c0fa6dc7992
Prasanna <prasanna.m.loa@gmail.com>
2026-08-02 07:06:58 +0000

    Remove unnecessary node exporter affinity override

--- modules/observability/main.tf
+++ modules/observability/main.tf
@@ -99,25 +99,6 @@ resource "helm_release" "kube_prometheus_stack" {
           size             = "10Gi"
         }
       }
-      "prometheus-node-exporter" = {
-        affinity = {
-          nodeAffinity = {
-            requiredDuringSchedulingIgnoredDuringExecution = {
-              nodeSelectorTerms = [
-                {
-                  matchExpressions = [
-                    {
-                      key      = "kubernetes.io/os"
-                      operator = "In"
-                      values   = ["linux"]
-                    }
-                  ]
-                }
-              ]
-            }
-          }
-        }
-      }
     })
   ]
 
