f2fb82f54fc600af77657abf7ea30e00ef787747
Prasanna <prasanna.m.loa@gmail.com>
2026-08-04 18:48:31 +0000

    Allow Velero to use backup KMS key

--- modules/velero/main.tf
+++ modules/velero/main.tf
@@ -88,7 +88,7 @@ resource "aws_iam_policy" "this" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
+    Statement = concat([
       {
         Effect = "Allow"
         Action = [
@@ -119,7 +119,17 @@ resource "aws_iam_policy" "this" {
         ]
         Resource = "*"
       }
-    ]
+      ], var.kms_key_arn == null ? [] : [
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:GenerateDataKey"
+        ]
+        Resource = var.kms_key_arn
+      }
+    ])
   })
 
   tags = var.tags
